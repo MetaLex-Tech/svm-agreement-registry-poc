e95ee98c6bdc2f93576b23224669e3e2364c4da8
detoo <daily.detoo@gmail.com>
2025-09-04 19:49:54 -0700

    feat: with simple input arguments

--- programs/svm-agreement-registry/src/lib.rs
+++ programs/svm-agreement-registry/src/lib.rs
@@ -6,11 +6,61 @@ declare_id!("VnpPHJweU4dLG6BATCh6MnaS1Y81hTC2esBpxMpDzg6");
 pub mod svm_agreement_registry {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn store_data(ctx: Context<StoreData>, kv_pairs: Vec<KeyValuePair>, signature: [u8; 64]) -> Result<()> {
+        msg!("store_data from: {:?}", ctx.program_id);
+
+        let data_entry = &mut ctx.accounts.data_entry;
+        data_entry.signature = signature;
+
+        // let data_entry = &mut ctx.accounts.data_entry;
+        // data_entry.signer = ctx.accounts.signer.key();
+        // data_entry.kv_pairs = kv_pairs;
+        // data_entry.signature = signature;
+        
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+// #[instruction(kv_pairs: Vec<(String, String)>)]
+pub struct StoreData<'info> {
+    // #[account(
+    //     init_if_needed,
+    //     payer = signer,
+    //     space = 8 + 32 + 64 + 4 + kv_pairs.len() * (4 + 32 + 4 + 32), // Discriminator + signer + sig + vec len + per pair (str len + data + str len + data); assuming max str len 32
+    //     seeds = [b"data", signer.key().as_ref()],
+    //     bump
+    // )]
+    // pub data_entry: Account<'info, DataEntry>,
+    // #[account(mut)]
+    // pub signer: Signer<'info>,
+    // pub system_program: Program<'info, System>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + 64
+    )]
+    pub data_entry: Account<'info, DataEntry>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct DataEntry {
+    pub signature: [u8; 64],
+}
+
+#[derive(Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct KeyValuePair {
+    pub key: String,
+    pub value: String,
+}
+
+// #[account]
+// pub struct DataEntry {
+//     pub signer: Pubkey,
+//     pub kv_pairs: Vec<(String, String)>,
+//     pub signature: [u8; 64],
+// }
--- tests/svm-agreement-registry.ts
+++ tests/svm-agreement-registry.ts
@@ -1,16 +1,39 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { Program, web3 } from "@coral-xyz/anchor";
 import { SvmAgreementRegistry } from "../target/types/svm_agreement_registry";
 
 describe("svm-agreement-registry", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.svmAgreementRegistry as Program<SvmAgreementRegistry>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  it("stores data", async () => {
+    const kvPairs: {key: string, value: string}[] = [
+      { key: "name", value: "Alice"},
+      { key: "age", value: "30"},
+    ];
+
+    console.log("provider.wallet.publicKey:", provider.wallet.publicKey);
+
+    // Generate keypair for the new account
+    const newAccountKp = new web3.Keypair();
+    console.log("newAccountKp.publicKey:", newAccountKp.publicKey);
+    const signature = Array.from({ length: 64 }, (_, index) => index);
+
+    const tx = await program.methods
+      .storeData(
+        kvPairs,
+        signature,
+      )
+      .accounts({
+        dataEntry: newAccountKp.publicKey,
+        signer: provider.wallet.publicKey,
+      })
+      .signers([newAccountKp])
+      .rpc();
+
     console.log("Your transaction signature", tx);
   });
 });
